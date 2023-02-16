fee3dce2eb8887a4a18dc60009993442d551b0c7
HLsnow12 <125589481+HLsnow12@users.noreply.github.com>
2023-02-16 14:19:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 
 import streamlit
-streamlit.title ('My parents healthy Meal')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.title ('My parents healthy Meal 🥣 ')
+streamlit.header('Breakfast Menu 🥗 ')
+streamlit.text('Omega 3 & Blueberry Oatmeal🥑🍞')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg🐔')
+ 
