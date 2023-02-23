0f24a4f4fbe0afc42ed9d5c99749dda674d4aff7
HLsnow12 <125589481+HLsnow12@users.noreply.github.com>
2023-02-23 11:21:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ from urllib.error import URLError
 
 streamlit.header('Fruityvice Fruity Advice')
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
      streamlit.error("Please select a fruit")
    else:
