7e48546c2b9d62ed2519c00bce3a460a56fc7d4a
HLsnow12 <125589481+HLsnow12@users.noreply.github.com>
2023-02-23 11:11:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,4 +73,10 @@ def get_fruityvice_data(this_fruit_choice):
   return fruityvice_normalized
  
 streamlit.header('Fruityvice Fruity Advice')
-    
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+     streamlit.error("Please select a fruit")
+   else:
+     back_from_function = get_fruityvice_data(fruit_choice)
+     streamlit.dataframe(back_from_function)
