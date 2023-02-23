27d90af9a1a9625165b6888cd9c98a093c6b9918
HLsnow12 <125589481+HLsnow12@users.noreply.github.com>
2023-02-23 11:08:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ from urllib.error import URLError
 # streamlit.error()
 
 #Creating a function
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
   fruityviceresponse = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
